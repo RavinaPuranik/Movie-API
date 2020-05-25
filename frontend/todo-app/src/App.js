165b907e81bcd7c8c189104f9f700a307a415768
Ravina <ravinapuranik1@gmail.com>
2020-05-25 21:00:11 +0530

    counter

---
+++ frontend/todo-app/src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import CounterButton from './components/counter/Counter';
+import Counter from './components/counter/Counter';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+      <Counter/>
+      </div>
+    );
+  }
+}
+
+export default App;
