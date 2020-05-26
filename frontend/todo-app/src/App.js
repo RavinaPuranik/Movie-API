7601490c58059271df34fd211302291fbb24eb35
Ravina <ravinapuranik1@gmail.com>
2020-05-26 18:46:36 +0530

    new components and styling

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,13 +1,13 @@
 import React,{Component} from 'react';
-import CounterButton from './components/counter/Counter';
-import Counter from './components/counter/Counter';
+import TodoApp from './components/todo/TodoApp';
 import './App.css';
+import './bootstrap.css';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-      <Counter/>
+      <TodoApp/>
       </div>
     );
   }
