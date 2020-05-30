ea5ac717171bc1d16c32121bb4ec31f045502f54
Ravina <ravinapuranik1@gmail.com>
2020-05-30 18:25:55 +0530

    frontend

---
+++ frontend/todo-app/src/api/todo/HelloWorldService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+class HelloWorldService {
+  executeHelloWorldService() {
+    return axios.get("http://localhost:8080/hello-world");
+  }
+
+  executeHelloWorldBeanService() {
+    return axios.get("http://localhost:8080/hello-world-bean");
+  }
+
+  executeHelloWorldPathVariableService(name) {
+    //let username='admin'
+    //let password='root'
+    //let basicAuthHeader='Basic '+window.btoa(username+":"+password)
+    return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`)
+      //,{
+      //headers:{
+      //  authorization:basicAuthHeader
+      //}
+    //}
+//  );
+  }
+}
+export default new HelloWorldService();
